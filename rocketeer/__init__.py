f5a6d72a0d037fae6a2da10a1672ff70b6140883
Moritz Wundke <b.thax.dcg@gmail.com>
2017-11-23 23:17:34 +0100

    Adding base entry point

--- rocketeer/__init__.py
+++ rocketeer/__init__.py
@@ -1,8 +1,13 @@
 import sys
 from pslib.tools import main_tool
 
+__version__ = "0.1"
+__author__ = "Moritz Wundke"
+__copyright__ = "(c) 2017 Moritz Wundke"
+__description__ = "Welcome Rocketeer, a simple script to build your Unreal Engine 4 Rocket distros! - v{version} {copyright} ({author})"
+
 def main():
-    return run_tools(sys.argv[1:])
+    return main_tool(sys.argv[1:], description=__description__, author=__author__, copyright=__copyright__, version=__version__)
 
 if __name__ == "__main__":
     sys.exit(main())
